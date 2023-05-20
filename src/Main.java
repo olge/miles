6959892261c71ddef11b320251ce83b0f74b5b0c
Olga Test <olka25@list.ru>
2023-05-20 13:24:14 +0300

    BonusMilesService.java added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 4990; // стоимость билета
-        int rubles = 20; // количество рублей для получения одной мили
-        int bonusMiles = ticketPrice / rubles; // количество бонусных миль за купленный билет
-
-        System.out.println(("Вы получили " + bonusMiles) + " миль");
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
